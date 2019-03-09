f801e90a3fbdcb2b1a249581a5ff8f3cce213621
ApokaliseGames <apokalisegames@gmail.com>
2019-03-09 20:35:09 -0300

    Start delete functionality

--- androidjava/app/src/main/java/sample/populator/DebtPopulator.java
+++ androidjava/app/src/main/java/sample/populator/DebtPopulator.java
@@ -1,6 +1,9 @@
 package sample.populator;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
+import java.util.TimeZone;
 
 import sample.model.Debt;
 import sample.view.DebtCreateView;
@@ -10,8 +13,10 @@ public class DebtPopulator {
 
     public Debt populate(DebtCreateView source) {
         Debt debt = new Debt();
-//        debt.setDate(source.date.getDate());
-        debt.setDate(LocalDateTime.now().toString());
+        LocalDateTime date =
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(source.date.getDate()),
+                        TimeZone.getDefault().toZoneId());
+        debt.setDate(date.toString());
         debt.setName(source.name.getText().toString());
         debt.setCategory(source.category.getText().toString());
         debt.setSubcategory(source.subcategory.getText().toString());
@@ -22,7 +27,7 @@ public class DebtPopulator {
     }
 
     public void populate(Debt source, DebtItemView target) {
-//        target.date.setText(source.getDate().toString());
+        target.date.setText(source.getDate().toString());
         target.name.setText(source.getName());
         target.category.setText(source.getCategory());
         target.subcategory.setText(source.getSubcategory());
